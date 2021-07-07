463a0591e3d0f5a0a6b5e049c9cc6b5d3814a225
hilmiAzman <hilmi.azman@srglobal.com>
2021-07-07 20:41:11 +0800

    Add a list component, add divider in the list

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,6 +1,33 @@
 <template>
-  <div class="home pa-6">
-    <h1>Todo Page</h1>
+  <div class="home">
+    <v-list
+      flat
+      class="pt-0"
+    >
+      
+      <div
+        v-for="task in tasks"
+        :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
+
+
+  
+    </v-list>
+
+
   </div>
 </template>
 
@@ -8,6 +35,24 @@
 
   export default {
     name: 'Home',
+    data(){
+      return {
+        tasks: [
+          {
+            id: 1,
+            title: 'Wake up',
+          },
+          {
+            id: 2,
+            title: 'Take a shower',
+          },
+          {
+            id: 3,
+            title: 'Make Breakfast',
+          },
+        ],
+      }
+    },
     
   }
 </script>
